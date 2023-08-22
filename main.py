39641b80326431ddddca830290496c52c984dcfc
Vini Y <vincentyu9603@gmail.com>
2023-08-22 21:34:40 +1200

    git ignore

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from utils.ImageToText import text_from_image
 from PIL import Image
+
 if __name__ == "__main__":
     image_path = "/home/devbox/refrigeratorMonitor/tests/ImageTotext/test1.jpg"
-    img = Image.open(image_path) 
-    fileName = "/home/devbox/refrigeratorMonitor/text"
+    img = Image.open(image_path)
+    fileName = "/home/devbox/refrigeratorMonitor/results/outputText/text"
     text_from_image(img, fileName)
